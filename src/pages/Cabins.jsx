57ed7bc2fd3d2878b1dbd73536bb14585bacdfd1
VU DUC TRI JOSEPH <vutrilkvn89@gmail.com>
2025-07-18 15:04:51 +0700

    fetch cabin data and display on cabins page, set deleteCabin funtion on aipCabins file.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
-
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://nywmlqwstwsgkqvbklbs.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg"
-        alt="cabin in the woods"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
-
 export default Cabins;
